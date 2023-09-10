7a265cc26b6ee7ae7e829a421210013613c7d62b
mishra077 <mishraab@usc.edu>
2023-09-10 01:15:03 -0700

    Complete Website

--- components/EmailSection.jsx
+++ components/EmailSection.jsx
@@ -47,7 +47,7 @@ const EmailSection = () => {
 
   return <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative" id="#contact">
     <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
             {" "}
             I&apos;m currently looking for new opportunities, my inbox is always open. 
